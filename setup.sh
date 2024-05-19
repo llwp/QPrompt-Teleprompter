257f70d807ff1d3afdf7c48059c9617b05e73633
Javier O. Cordero Pérez <javier.cordero@kdab.com>
2024-05-19 18:38:19 -0400

    A hard-coded attempt at getting QPrompt to work with WASM

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ usage: $0 <CMAKE_BUILD_TYPE> <CMAKE_PREFIX_PATH> [<QT_MAJOR_VERSION> | ] [CLEAR 
 
 Defaults:
  * CMAKE_BUILD_TYPE: "Release"
- * CMAKE_PREFIX_PATH: "~/Qt/6.6.1/gcc_64/"
+ * CMAKE_PREFIX_PATH: "~/Qt/6.7.0/gcc_64/"
  * QT_MAJOR_VERSION: 6
 
 Setup script for building QPrompt
@@ -36,13 +36,16 @@ This script assumes you've already installed the following dependencies:
  * Qt 6 Open Source
 EOF
 
+source "/home/javier/Software/emsdk/emsdk_env.sh"
+EMROOT="/home/javier/Software/emsdk/upstream/emscripten"
+
 CMAKE_BUILD_TYPE=$1
 if [ "$CMAKE_BUILD_TYPE" == "" ]
         then CMAKE_BUILD_TYPE="Release"
 fi
 CMAKE_PREFIX_PATH=$2
 if [ "$CMAKE_PREFIX_PATH" == "" ]
-        then CMAKE_PREFIX_PATH="~/Qt/6.6.1/gcc_64/"
+        then CMAKE_PREFIX_PATH="~/Qt/6.7.0/wasm_multithread"
 fi
 QT_MAJOR_VERSION=$3
 if [ "$QT_MAJOR_VERSION" == "" ]
@@ -62,9 +65,15 @@ else
     CLEAR_ALL=false
 fi
 
+CLEAR_ALL=true
+
 # Constants
-CMAKE_INSTALL_PREFIX="./build/dist"
-ARCHITECTURE="$(uname -m)"
+ECM_ADDITIONAL_FIND_ROOT_PATH="/home/javier/Development/Teleprompters/qprompt/QPrompt/build"
+CMAKE_FIND_ROOT_PATH="/home/javier/Development/Teleprompters/qprompt/QPrompt/build"
+CMAKE_INSTALL_PREFIX="/home/javier/Development/Teleprompters/qprompt/QPrompt/build/dist"
+ECM_DIR="${CMAKE_INSTALL_PREFIX}/share/ECM/cmake"
+# QT_INSTALL_PREFIX="~/Qt/6.7.0/wasm_multithread"
+# QT_INSTALL_PREFIX="~/Qt/6.7.0/wasm_multithread/bin/qmake"
 
 echo "Build directory is ./build"
 if $CLEAR_ALL # QPrompt and dependencies
@@ -76,40 +85,39 @@ elif $CLEAR # QPrompt
 fi
 mkdir -p build install
 
-echo "Downloading git submodules"
-git submodule update --init --recursive
+# echo "Downloading git submodules"
+# git submodule update --init --recursive
 
-echo "Extra CMake Modules"
+echo "Extra CMake Modules"/cmake/Modules/Platform/
 if $CLEAR_ALL
     then
     rm -dRf ./3rdparty/extra-cmake-modules/build
 fi
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -B ./3rdparty/extra-cmake-modules/build ./3rdparty/extra-cmake-modules/
-cmake --build ./3rdparty/extra-cmake-modules/build
+emcmake cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DBUILD_HTML_DOCS=OFF -DBUILD_MAN_DOCS=OFF -DBUILD_TESTING=OFF -DCMAKE_TOOLCHAIN_FILE=/home/javier/Software/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -B ./3rdparty/extra-cmake-modules/build ./3rdparty/extra-cmake-modules/
+emake make ./3rdparty/extra-cmake-modules/build
 cmake --install ./3rdparty/extra-cmake-modules/build
 
+echo "$ECM_DIR"
+
+# -DCMAKE_PREFIX_PATH "${PROJECT_SOURCE_DIR}/build/CMakeFiles/pkgRedirects")
+# CMAKE_PREFIX_PATH=~/Qt/6.7.0/wasm_multithread/lib/cmake/Qt6Core:$CMAKE_PREFIX_PATH
+CMAKE_MODULE_PATH=$CMAKE_PREFIX_PATH
+# Qt6Core_DIR=~/Qt/6.7.0/wasm_multithread/lib/cmake/Qt6Core/
+
 # KDE Frameworks
-for dependency in ./3rdparty/k*; do
+# for dependency in ./3rdparty/k*; do
+for dependency in ./3rdparty/kcoreaddons; do
     echo $dependency
     if $CLEAR_ALL
     then
-        rm -dRf ./$dependency/build
+        rm -dRf $dependency/build
     fi
-    cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -B ./$dependency/build ./$dependency/
-    cmake --build ./$dependency/build
-    cmake --install ./$dependency/build
+    /home/javier/Qt/6.7.0/wasm_multithread/bin/qt-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DECM_DIR=$ECM_DIR -DQt6Core_DIR=$Qt6Core_DIR -DLIB_INSTALL_DIR=$QT_INSTALL_PREFIX -DKF_IGNORE_PLATFORM_CHECK=ON -DCMAKE_TOOLCHAIN_FILE=/home/javier/Software/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -B $dependency/build $dependency
+    /home/javier/Qt/6.7.0/wasm_multithread/bin/qt-cmake --build $dependency/build
+    /home/javier/Qt/6.7.0/wasm_multithread/bin/qt-cmake --install $dependency/build
 done
 
-echo "QHotkey"
-if $CLEAR_ALL
-then
-    rm -dRf ./3rdparty/QHotkey/build
-fi
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DBUILD_SHARED_LIBS=ON -DQT_DEFAULT_MAJOR_VERSION=${QT_MAJOR_VERSION} -B ./3rdparty/QHotkey/build ./3rdparty/QHotkey/
-cmake --build ./3rdparty/QHotkey/build
-cmake --install ./3rdparty/QHotkey/build
-
-echo "QPrompt"
-cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -B ./build .
-cmake --build ./build
-cmake --install ./build
+# echo "QPrompt"
+# emcmake cmake -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX -DECM_KDE_MODULE_DIR=$ECM_DIR -DCMAKE_TOOLCHAIN_FILE=/home/javier/Software/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -B ./build .
+# emake make ./build
+# cmake --install ./build
